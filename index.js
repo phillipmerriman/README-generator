6c7c41f499d8a2e982c376e1751ab0ff51f0e9c9
phillipmerriman <phillipmerriman@gmail.com>
2020-11-24 19:01:48 -0600

    started editing markdown of readme template

--- index.js
+++ index.js
@@ -46,35 +46,24 @@ inquirer.prompt([
     }
 
 ]).then(response => fs.writeFile("README1.md", 
-    `${response.title}
+    `#${response.title}
 
-    Description
+    ##Description
     ${response.description}
     
-    Table of Contents (Optional)
+    ##Table of Contents (Optional)
     If your README is very long, add a table of contents to make it easy for users to find what they need.
     
-    Installation
+    ##Installation
     ${response.installation}
-    Usage
+    ##Usage
     ${response.usageInfo}
-    Credits
-    
-    
-    
-    
-    Installation
-    ${response.installation}
-    
-    Usage
-    ${response.usageInfo}
-    
-    Credits
+    ##Credits
     List your collaborators, if any, with links to their GitHub profiles.
     If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
     If you followed tutorials, include links to those here as well.
     
-    License
+    ##License
     ${response.license} 
     
     🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
@@ -83,10 +72,10 @@ inquirer.prompt([
     
     Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by shields.io. You may not understand what they all represent now, but you will in time.
     
-    Contributing
+    ##Contributing
     ${response.contributionGuidelines}   
 
-    Tests
+    ##Tests
     ${response.testInstructions}
 
     © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.`, err => err ? console.error(err) : console.log("hip, hip, horray!")
